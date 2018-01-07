4a4f50cf4ebfbfffaffed90024306147ddcd2a12
Sharath <sharath.kudikyala@hotmail.com>
2018-01-07 16:09:22 +0530

    Hello World program difference between print() and println()

---
+++ src/HelloWorld.kt
@@ -0,0 +1,10 @@
+//Hello World Program
+
+fun main(args: Array<String>) {
+    // print(): prints given message
+    print("Hello World")
+
+    //println() : prints given message and new line
+    println("Welcome to Kotlin")
+    println("Printed message in a new/next line")
+}
